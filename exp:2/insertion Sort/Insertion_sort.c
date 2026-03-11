045c4bbab9095f80b14059cb799ee20fd6a8b7ad
ratishakhadse <ratisha.khadse.batch2025@sitnagpur.siu.edu.in>
2026-03-11 22:17:01 +0530

    Update Insertion_sort.c

--- exp:2/insertion Sort/Insertion_sort.c
+++ exp:2/insertion Sort/Insertion_sort.c
@@ -1,1 +1,33 @@
+/*Write a program in C that implements the Insertion sort to sort a given array of integers in ascending order.*/
+
+#include <stdio.h>
+
+int main() {
+    int a[20], n, i, j, key;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Insertion Sort Logic
+    for (i = 1; i < n; i++) {
+        key = a[i];
+        j = i - 1;
+
+        while (j >= 0 && a[j] > key) {
+            a[j + 1] = a[j];
+            j--;
+        }
+
+        a[j + 1] = key;
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
 
