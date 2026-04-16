a150a96f9205b0c5bd93e9816dc2cb4367518dea
ratishakhadse <ratisha.khadse.batch2025@sitnagpur.siu.edu.in>
2026-04-16 10:47:13 +0530

    Add DFS implementation for directed graph
    Implement Depth-First Search (DFS) for a directed graph using an adjacency list. The program reads vertices and edges, and prints reachable vertices from a starting vertex.

--- exp:9/DFS.c
+++ exp:9/DFS.c
@@ -1,1 +1,62 @@
+/*Write a C program to implement and perform a Depth-First Search (DFS) on a directed graph represented using an adjacency list.
+The program should print all vertices reachable from a given starting vertex in the order they are visited by DFS.*/
 
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+int main() {
+    int V, E;
+    int u, v, start;    
+    struct Node {
+        int vertex;
+        struct Node* next;
+    };
+    struct Node* adj[MAX];
+    int visited[MAX];
+    scanf("%d %d", &V, &E);
+    for (int i = 0; i < V; i++) {
+        adj[i] = NULL;
+        visited[i] = 0;
+    }
+    for (int i = 0; i < E; i++) {
+        scanf("%d %d", &u, &v);
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->vertex = v;
+        newNode->next = adj[u];
+        adj[u] = newNode;
+    }
+    for (int i = 0; i < V; i++) {
+        struct Node *p, *q;
+        for (p = adj[i]; p != NULL; p = p->next) {
+            for (q = p->next; q != NULL; q = q->next) {
+                if (p->vertex > q->vertex) {
+                    int temp = p->vertex;
+                    p->vertex = q->vertex;
+                    q->vertex = temp;
+                }
+            }
+        }
+    }
+ scanf("%d", &start);
+    int stack[MAX], top = -1;
+    stack[++top] = start;
+    while (top != -1) {
+        int curr = stack[top--];
+        if (!visited[curr]) {
+            printf("%d ", curr);
+            visited[curr] = 1;
+        }
+        struct Node* temp = adj[curr];
+        int tempArr[MAX], k = 0;
+        while (temp != NULL) {
+            if (!visited[temp->vertex]) {
+                tempArr[k++] = temp->vertex;
+            }
+            temp = temp->next;
+        }
+        for (int i = k - 1; i >= 0; i--) {
+            stack[++top] = tempArr[i];
+        }
+    }
+    return 0;
+}
